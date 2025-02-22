8c7589d5f2ecf20c005b3f91025ed483e051a471
Darvin <darvin@MacBook-Pro.local>
2025-02-22 12:46:45 -0800

    add api2

--- api/index.py
+++ api/index.py
@@ -6,3 +6,7 @@ app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
+
+@app.get("/api/py/helloFastApi2")
+def hello_fast_api2():
+    return {"message": "Hello from FastAPI 2"}
